4f529a823662c4570fa281c16bf215a2d6e42198
Rohan Poudel <nsrapoudel@gmail.com>
2023-07-12 11:42:24 +0545

    Added swiper to home hero component

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,7 @@ export default function Home() {
       <LatestUpdates />
       <TibetTour />
       <UseFull />
+      <Hero />
     </div>
   )
 }
--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -1,28 +1,112 @@
+"use client"
 import Image from "next/image";
 import styles from "./hero.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+
+import "swiper/css"
+import "swiper/css/navigation";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <Image
-        src="/images/hero.jpeg"
-        width={1920}
-        height={1080}
-        alt="Hero Image"
-        className={styles.image}
-      />
-      <div className={styles.details}>
-        <h1 className={styles.countryName}>
-          Nepal
-        </h1>
-        <h3 className={styles.tagline}>
-          8th wonder of the world
-        </h3>
-        <button className={styles.exploreBtn}>
-          Explore
-        </button>
-      </div>
-    </div>
+    <Swiper
+      navigation={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false
+      }}
+      modules={[Navigation, Autoplay]}
+      className={styles.swiper}
+    >
+      <SwiperSlide>
+        <div className={styles.hero}>
+          <Image
+            src="/images/hero.jpeg"
+            width={1920}
+            height={1080}
+            alt="Hero Image"
+            className={styles.image}
+          />
+          <div className={styles.details}>
+            <h1 className={styles.countryName}>
+              Nepal
+            </h1>
+            <h3 className={styles.tagline}>
+              8th wonder of the world
+            </h3>
+            <button className={styles.exploreBtn}>
+              Explore
+            </button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.hero}>
+          <Image
+            src="/images/hero.jpeg"
+            width={1920}
+            height={1080}
+            alt="Hero Image"
+            className={styles.image}
+          />
+          <div className={styles.details}>
+            <h1 className={styles.countryName}>
+              Nepal
+            </h1>
+            <h3 className={styles.tagline}>
+              8th wonder of the world
+            </h3>
+            <button className={styles.exploreBtn}>
+              Explore
+            </button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.hero}>
+          <Image
+            src="/images/hero.jpeg"
+            width={1920}
+            height={1080}
+            alt="Hero Image"
+            className={styles.image}
+          />
+          <div className={styles.details}>
+            <h1 className={styles.countryName}>
+              Nepal
+            </h1>
+            <h3 className={styles.tagline}>
+              8th wonder of the world
+            </h3>
+            <button className={styles.exploreBtn}>
+              Explore
+            </button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.hero}>
+          <Image
+            src="/images/hero.jpeg"
+            width={1920}
+            height={1080}
+            alt="Hero Image"
+            className={styles.image}
+          />
+          <div className={styles.details}>
+            <h1 className={styles.countryName}>
+              Nepal
+            </h1>
+            <h3 className={styles.tagline}>
+              8th wonder of the world
+            </h3>
+            <button className={styles.exploreBtn}>
+              Explore
+            </button>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
