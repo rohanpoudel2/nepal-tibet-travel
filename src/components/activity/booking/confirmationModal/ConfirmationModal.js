8ed13059450ca6de1096541004b92196d33e3faa
Rohan Poudel <nsrapoudel@gmail.com>
2023-11-25 15:31:25 +0545

    Updated Confirmation Modal Design

--- src/components/activity/booking/confirmationModal/ConfirmationModal.js
+++ src/components/activity/booking/confirmationModal/ConfirmationModal.js
@@ -3,6 +3,8 @@ import styles from "./confirmationModal.module.scss";
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Link from "next/link";
+import Image from "next/image";
+import BookingImage from "@/../public/images/checkmark.svg";
 
 const style = {
   position: 'absolute',
@@ -40,43 +42,27 @@ const ConfirmationModal = ({ open, setOpen, duration, name }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <div className="relative w-full max-w-2xl max-h-full">
-            <div className="relative bg-white rounded-lg shadow ">
-              <div className="flex items-start justify-between p-4 border-b rounded-t ">
-                <h3 className="text-xl font-semibold text-gray-900 ">
-                  Get Instant Confirmation on your Booking
-                </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="static-modal" onClick={() => setOpen(false)}>
-                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                  </svg>
-                </button>
+          <div className="relative max-h-full flex items-center justify-center px-5 py-10 w-[80vw] md:w-[50vh] md:py-24">
+            <div className="flex items-center justify-center flex-col gap-5 text-center">
+              <Image
+                src={BookingImage}
+                alt="Blue Booking Icon"
+                width={100}
+                height={100}
+              />
+              <span className="font-medium text-gray-700">Do you want to confirm your booking for <br /><span className="font-semibold">{name}</span>?</span>
+              <div className="text-xl font-semibold space-x-2">
+                <span>{formatDateString(parseDate(open.bookingDate || '1/1/1990'))}</span>
+                <span className="text-gray-500 font-medium">|</span>
+                <span>{formattedDate}</span>
               </div>
-              <div className="p-6 space-y-2 h-[50%] overflow-y-scroll">
-                <div>
-                  <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
-                    {name}
-                  </h2>
-                </div>
-                <div className="flex flex-row gap-10 items-center justify-center md:gap-30 p-2 transition-colors duration-300 transform rounded-lg bg-gray-50 ">
-                  <div>
-                    <h4 className="mt-2 text-xl md:text-3xl font-semibold text-green-500 ">$1820.00
-                      <span className="text-sm md:text-base font-medium text-gray-500 ">/per person</span></h4>
-                  </div>
-                  <div className="bg-white rounded-lg p-2 flex gap-5 items-center justify-between mt-5 w-fit">
-                    <span className="w-10">{formatDateString(parseDate(open.bookingDate || '1/1/1990'))}</span>
-                    <div className="border-2 rounded-full flex items-center justify-center p-1 w-6 h-6">
-                      <i className="fa-solid fa-arrow-right w-5" />
-                    </div>
-                    <span className="w-10">{formattedDate}</span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b ">
-                <Link href="/booking">
-                  <button data-modal-hide="static-modal" type="button" className="text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Continue</button>
-                </Link>
-                <button data-modal-hide="static-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-sky-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10" onClick={() => setOpen(false)}>Go back</button>
+              <Link href="/booking">
+                <Button className="rounded-xl bg-sky-500 px-4 py-2 text-xl font-semibold text-white hover:bg-sky-600 focus:ring focus:ring-sky-200">Continue</Button>
+              </Link>
+              <div className="absolute top-5 right-5 bg-sky-500 p-2 text-lg rounded text-white cursor-pointer  hover:bg-sky-600" onClick={() => setOpen(false)}>
+                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
               </div>
             </div>
           </div>
