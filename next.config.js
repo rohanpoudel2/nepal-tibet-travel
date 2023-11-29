93d3f96f6b3314cfcfbca456df06ce6daa2b9395
Rohan Poudel <nsrapoudel@gmail.com>
2023-11-29 12:56:57 +0545

    next config image domain changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "images.pexels.com", "images.unsplash.com", "1.gravatar.com", "https://wp.nepaltibettravel.com"]
+    domains: [
+      "localhost",
+      "images.pexels.com",
+      "images.unsplash.com",
+      "1.gravatar.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wp.nepaltibettravel.com',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+    ],
   },
 }
 
